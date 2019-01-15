9520b554b054605cf175ea3bfb0d5027520103ac
Pascal Welke <welke@uni-bonn.de>
2019-01-15 19:07:18 +0100

    removal of my own OneHotFrequentSubtreeFeatures classes.
    It is interesting to note that the sklearn LinearRegression algorithm
    is faster *and produces less error* when applied to the sparse OneHot
    features produced by the sklearn class applied to the data generated
    by my FeatureGeneratingTree class compared to the same alg applied to
    dense dataset produced by my OneHotFeatureGeneratingTree class.
    
    This is the case, although the data matrices contain identical data.

--- FeatureGenerators/DecisionSnippetFeatures.py
+++ FeatureGenerators/DecisionSnippetFeatures.py
@@ -91,33 +91,6 @@ class FeatureGeneratingTree(Tree.Tree):
            
     def get_features_batch(self, X):
         return np.array([self.get_features(x) for x in X])
-    
-
-class OneHotFeatureGeneratingTree(Tree.Tree):
-    
-    def __init__(self, pattern):
-        super(OneHotFeatureGeneratingTree, self).__init__()
-        self.fromJSON(makeProperBinaryDT(pattern))
-        self.n_nodes = len(self.nodes.keys())
-   
-    def get_features(self, x):
-        curNode = self.head
-
-        # walk through the partial decision tree as long as possible
-        while(curNode.prediction is None):            
-            if (x[curNode.feature] <= curNode.split): 
-                curNode = curNode.leftChild
-            else:
-                curNode = curNode.rightChild
-         
-        features = np.zeros(self.n_nodes)
-        features[curNode.id] = 1
-        return features
-
-    def get_features_batch(self, X):
-        return np.vstack([self.get_features(x) for x in X])
-
-
 
 
 # we want to build a feature generator for the input data that is based on frequent subtrees of the random forests 
@@ -140,19 +113,4 @@ class FrequentSubtreeFeatures():
     def fit_transform(self, X, y=None):
         self.fit(X, y)
         return self.transform(X)
-    
-    
-class OneHotFrequentSubtreeFeatures():
-    def __init__(self, patterns=None):
-        self.patterns = [OneHotFeatureGeneratingTree(pattern) for pattern in patterns]
-        self.n_features = sum([p.n_nodes for p in self.patterns])       
-    
-    def fit(self, X=None, y=None):
-        pass
-    
-    def transform(self, X):
-        return np.hstack([pattern.get_features_batch(X) for pattern in self.patterns])     
-    
-    def fit_transform(self, X, y=None):
-        self.fit(X, y)
-        return self.transform(X)
+
