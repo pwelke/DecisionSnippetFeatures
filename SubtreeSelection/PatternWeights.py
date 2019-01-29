8b86d30de4a1a9cb7eba610481b5edd7f0d355bd
Pascal Welke <welke@uni-bonn.de>
2019-01-29 13:50:16 +0100

    add new constructor for PatternStatistics class that finds the pattern embeddings by itself
    now, for a set of (frequent) patterns and a transaction database, both
    in json format, we can do statistics directly. Before this commit, we
    would need to use findEmbeddings.py to create a file where the
    transactions are annotated with the embedding information.

--- SubtreeSelection/PatternWeights.py
+++ SubtreeSelection/PatternWeights.py
@@ -1,4 +1,7 @@
 import json
+
+# import from relative path
+from .findEmbeddings import annotateEmbeddings
 from collections import Counter
 
 
@@ -8,6 +11,12 @@ class PatternStatistics():
     counter = None
     weightFunction = None
     patternDict = None
+
+    def __init__(self, weightFunction):
+        if isinstance(weightFunction, str):
+            self.weightFunction = self.weightFunctions[weightFunction]
+        else:
+            self.weightFunction = weightFunction
 
     weightFunctions = {
         # weight each embedding with the data support of its root node, if the pattern has more than one vertex
@@ -19,29 +28,6 @@ class PatternStatistics():
         # this weight function tells us, how much vertices we could save in the RF by contracting each embedding of a pattern into a single vertex:
         'single_node_compression' : lambda p,t : 1 if len(p[1]) - 1 > 1 else 0
     }
-
-    def __init__(self, transactionFile, patternFile, weightFunction):
-        """Create an object that weights the embeddings (stored in transactionFile) of the patterns (stored in patternFile)
-        and outputs the most important / frequent / whatever patterns prettily.
-
-        weightFunction must either be a string that appears in PatternStatistics.weightFunctions or a function that must accept
-        - a pattern of the form (id, tree) where id is an int and tree is a tree in a partial Buschjäger et al. format.
-        - a transaction vertex (the root of the embedding being currently weighted)
-        as input and
-        - output an int.
-        """
-        if isinstance(weightFunction, str):
-            self.weightFunction = self.weightFunctions[weightFunction]
-        else:
-            self.weightFunction = weightFunction
-
-        f = open(patternFile)
-        self.patternDict = { pattern['patternid'] : pattern['pattern'] for pattern in json.load(f) }
-        f.close()
-
-        f = open(transactionFile)
-        self.transactions = json.load(f)
-        f.close()
 
     def __embeddingStatsRec(self, currentNode):
 
@@ -79,3 +65,43 @@ class PatternStatistics():
         self.embeddingStats()
         return [ {'id' : x[0], 'weight' : x[1], 'pattern' : self.patternDict[x[0]]} for x in self.counter.most_common(k) ]
 
+
+class PatternStatisticsFromPrecomputed(PatternStatistics):
+
+    def __init__(self, patternFile, embeddingFile, weightFunction):
+        """Create an object that weights the embeddings (stored in transactionFile) of the patterns (stored in patternFile)
+        and outputs the most important / frequent / whatever patterns prettily.
+
+        weightFunction must either be a string that appears in PatternStatistics.weightFunctions or a function that must accept
+        - a pattern of the form (id, tree) where id is an int and tree is a tree in a partial Buschjäger et al. format.
+        - a transaction vertex (the root of the embedding being currently weighted)
+        as input and
+        - output an int.
+        """
+        super(PatternStatisticsFromPrecomputed, self).__init__(weightFunction)
+
+        f = open(patternFile)
+        self.patternDict = { pattern['patternid'] : pattern['pattern'] for pattern in json.load(f) }
+        f.close()
+
+        f = open(embeddingFile)
+        self.transactions = json.load(f)
+        f.close()
+
+
+class PatternStatisticsFromSkratch(PatternStatistics):
+
+    def __init__(self, patternFile, transactionFile, weightFunction):
+        super(PatternStatisticsFromSkratch, self).__init__(weightFunction)
+
+        pf = open(patternFile, 'r')
+        patterns = json.load(pf)
+        pf.close()
+
+        tf = open(transactionFile, 'r')
+        self.transactions = json.load(tf)
+        tf.close()
+
+        self.patternDict = { pattern['patternid'] : pattern['pattern'] for pattern in patterns }
+
+        annotateEmbeddings(patterns, self.transactions)
